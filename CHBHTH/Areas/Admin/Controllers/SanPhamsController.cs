a8e855d098f22eb2a2714391b6aa220c86a5ccd0
decontn <trinhkien1012@gmail.com>
2024-07-13 21:58:18 +0700

    D2K project

---
+++ CHBHTH/Areas/Admin/Controllers/SanPhamsController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using CHBHTH.Models;
+
+namespace CHBHTH.Areas.Admin.Controllers
+{
+    public class SanPhamsController : Controller
+    {
+        private QLbanhang db = new QLbanhang();
+
+        // GET: SanPhams
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.LoaiHang).Include(s => s.NhaCungCap);
+            var u = Session["use"] as CHBHTH.Models.TaiKhoan;
+            if (u.PhanQuyen.TenQuyen == "Adminstrator")
+            {
+                return View(sanPhams.OrderByDescending(s => s.MaSP).ToList());
+            }
+            return RedirectPermanent("~/Home/Index");
+        }
+
+        // GET: SanPhams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLoai = new SelectList(db.LoaiHangs, "MaLoai", "TenLoai");
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC");
+            return View();
+        }
+
+        // POST: SanPham/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SanPham sanPham, HttpPostedFileBase AnhSP)
+        {
+            if (ModelState.IsValid)
+            {
+                if (AnhSP != null && AnhSP.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(AnhSP.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+
+                    // Kiểm tra và tạo thư mục nếu không tồn tại
+                    var directory = Path.GetDirectoryName(path);
+                    if (!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+
+                    AnhSP.SaveAs(path);
+                    sanPham.AnhSP = "/Images/" + fileName;
+                }
+
+                db.SanPhams.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLoai = new SelectList(db.LoaiHangs, "MaLoai", "TenLoai", sanPham.MaLoai);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaLoai = new SelectList(db.LoaiHangs, "MaLoai", "TenLoai", sanPham.MaLoai);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(SanPham sanPham, HttpPostedFileBase AnhSP)
+        {
+            if (ModelState.IsValid)
+            {
+                if (AnhSP != null && AnhSP.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(AnhSP.FileName);
+                    var path = Path.Combine(Server.MapPath("~/Images/"), fileName);
+
+                    // Kiểm tra và tạo thư mục nếu không tồn tại
+                    var directory = Path.GetDirectoryName(path);
+                    if (!Directory.Exists(directory))
+                    {
+                        Directory.CreateDirectory(directory);
+                    }
+
+                    AnhSP.SaveAs(path);
+                    sanPham.AnhSP = "/Images/" + fileName;
+                }
+
+                db.Entry(sanPham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaLoai = new SelectList(db.LoaiHangs, "MaLoai", "TenLoai", sanPham.MaLoai);
+            ViewBag.MaNCC = new SelectList(db.NhaCungCaps, "MaNCC", "TenNCC", sanPham.MaNCC);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SanPham sanPham = db.SanPhams.Find(id);
+            db.SanPhams.Remove(sanPham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UploadImage()
+        {
+            return View();
+        }
+
+        // POST: SanPham/UploadImage
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Images"), fileName);
+
+                // Kiểm tra và tạo thư mục nếu không tồn tại
+                var directory = Path.GetDirectoryName(path);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                file.SaveAs(path);
+
+                // Lưu trữ thông tin file vào cơ sở dữ liệu
+                var sanPham = new SanPham
+                {
+                    AnhSP = "/Images/" + fileName,
+                    // Thêm các thuộc tính khác của SanPham nếu cần
+                };
+                db.SanPhams.Add(sanPham);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CHBHTH/Controllers/sanphamController.cs
@@ -0,0 +1,106 @@
+﻿using CHBHTH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.IO;
+
+
+namespace CHBHTH.Controllers
+{
+    public class sanphamController : Controller
+    {
+        private QLbanhang db = new QLbanhang();
+        // GET: sanpham
+        public ActionResult Index()
+        {
+            var sanPhams = db.SanPhams.Include(s => s.LoaiHang).Include(s => s.NhaCungCap);
+            return View(sanPhams.ToList());
+        }
+
+        public ActionResult OP()
+        {
+            var ip = db.SanPhams.Where(n => n.MaLoai == 1).Take(4).ToList();
+            return PartialView(ip);
+        }
+        
+            public ActionResult SAS()
+        {
+            var ip = db.SanPhams.Where(n => n.MaLoai == 2).Take(4).ToList();
+            return PartialView(ip);
+        }
+
+        public ActionResult phukien()  
+        {
+            var ip = db.SanPhams.Where(n => n.MaLoai == 3).Take(4).ToList();
+            return PartialView(ip);
+        }
+
+        public ActionResult xemchitiet(int Masp = 0)
+        {
+            var chitiet = db.SanPhams.SingleOrDefault(n => n.MaSP == Masp);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+
+        public ActionResult IP()
+        {
+            var iphones = db.SanPhams.Where(s => s.LoaiHang.TenLoai == "Iphone").ToList(); // Tùy chỉnh điều kiện lọc sản phẩm của bạn nếu cần
+            return View(iphones);
+        }
+
+        
+
+       
+        public ActionResult xemchitietdanhmuc(int MaLoai)
+        {
+            var ip = db.SanPhams.Where(n => n.MaLoai == MaLoai).ToList();
+            return PartialView(ip);
+
+        }
+        public ActionResult UploadImage()
+        {
+            return View();
+        }
+
+        // POST: SanPham/UploadImage
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // Lưu trữ file vào thư mục trên server
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Images"), fileName);
+                file.SaveAs(path);
+
+                // Lưu trữ thông tin file vào cơ sở dữ liệu
+                var sanPham = new SanPham
+                {
+                    AnhSP = "/Images/" + fileName,
+                    // Thêm các thuộc tính khác của SanPham nếu cần
+                };
+                db.SanPhams.Add(sanPham);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
